be4ffbe1ec67acb9ecdab1547d144212ab002ff6
Isak Pao <windperson@gmail.com>
2024-09-09 12:27:48 +0800

    Fix declaration syntax

--- Demo1to1MappingTestModule/Production_test/FunctionVerify.ps1
+++ Demo1to1MappingTestModule/Production_test/FunctionVerify.ps1
@@ -1,9 +1,17 @@
 
-function VerifyParameters($commandInfo, $designedParamters) {
+function VerifyParameters {
+    <#
+    .SYNOPSIS
+    Verify if the function parameters are designed correctly.
+    #>
+    param(
+        [System.Management.Automation.CommandInfo]$commandInfo,
+        [System.Collections.Hashtable]$designedParameters
+    )
     # Note: since PowerShell's built-in Hashtable is case insensitive, we can't use it to exactly check function parameters
     $parameterTable = New-Object 'System.Collections.Hashtable'
-    foreach ($key in $designedParamters.Keys) {
-        $parameterTable.Add($key, $designedParamters[$key])
+    foreach ($key in $designedParameters.Keys) {
+        $parameterTable.Add($key, $designedParameters[$key])
     }
     
     foreach ($parameter in $commandInfo.Parameters.Values.GetEnumerator()) {
