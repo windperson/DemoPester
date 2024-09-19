7ab07307c6b8661b09d6cb230468a077c105a01f
Isak Pao <windperson@gmail.com>
2024-09-20 00:00:52 +0800

    Add functional tests

--- Demo1to1MappingTestModule/Production_test/ProductFeatures/DevelopUtil/ObjectUtils.Tests.ps1
+++ Demo1to1MappingTestModule/Production_test/ProductFeatures/DevelopUtil/ObjectUtils.Tests.ps1
@@ -24,3 +24,28 @@ Describe "Echo function API declaration" -Tag "EchoFeature", "FunctionDeclaratio
         VerifyApiDefinition -Name $Name -CommandType $CommandType 
     }
 }
+
+Describe "SafelyGetType()" -Tag "ObjectUtils"{
+
+    It "Should return null for null input" {
+        SafelyGetType $null | Should -BeNullOrEmpty
+    }
+
+    It "Should return type of input string" {
+        $testInput = "TestString"
+        $result = SafelyGetType -obj $testInput
+        $result | Should -BeExactly $testInput.GetType()
+    }
+
+    It "Should return type of input array" {
+        $testInput = @()
+        $result = SafelyGetType -obj $testInput
+        $result | Should -BeExactly $testInput.GetType()
+    }
+
+    It "Should return type of custom PSObject input " {
+        $testInput = [PSCustomObject]@{Name = "TestName"; Value = "TestValue"}
+        $result = SafelyGetType -obj $testInput
+        $result | Should -BeExactly $testInput.GetType()
+    }
+}
