ed8d6cab8e43775d2cf36f2eef89c5323c782b41
Isak Pao <windperson@gmail.com>
2024-10-08 00:48:53 +0800

    Add a triggy API input parameter name 'Type'

--- Demo1to1MappingTestModule/Production/ProductFeatures/DevelopUtil/ObjectUtils.ps1
+++ Demo1to1MappingTestModule/Production/ProductFeatures/DevelopUtil/ObjectUtils.ps1
@@ -1,3 +1,5 @@
+using namespace System.Diagnostics.CodeAnalysis
+
 function SafelyGetType {
     [OutputType([System.Type])]
     param($obj)
@@ -6,6 +8,44 @@ function SafelyGetType {
         if ($null -eq $obj) { return $null }
 
         return $obj.GetType()
+    }
+}
+
+function Dump-ObjectProps {
+    [SuppressMessage('PSUseApprovedVerbs', '')]
+    [SuppressMessage('PSUseSingularNouns', '')]
+    [OutputType([string[]])]
+    param(
+        [Parameter(Mandatory = $true)]
+        [object] $Value,
+        [Parameter(Mandatory = $false)]
+        [System.Type] $Type = $null,
+        [Parameter(Mandatory = $false)]
+        [string[]] $PropertiesFilter = @()
+    )
+
+    process {
+        if ($null -eq $Value) { return $null }
+        if ( $null -ne $Type -and $Type -isnot [PSCustomObject]) {
+            $typeNames = $Type.GetProperties() | Select-Object -ExpandProperty Name
+        }
+
+        return @( Get-Member -InputObject $Value -MemberType Properties | ForEach-Object {
+                # If the Type has that property, then get the value
+                if ($typeNames.Count -gt 0) {
+                    if ($typeNames -contains $_.Name) {
+                        "$($_.Name)=$($Value.$($_.Name))"
+                    }
+                }
+                elseif ($PropertiesFilter.Count -gt 0) {
+                    if ($PropertiesFilter -contains $_.Name) {
+                        "$($_.Name)=$($Value.$($_.Name))"
+                    }
+                }
+                else {
+                    "$($_.Name)=$($Value.$($_.Name))"
+                }
+            })
     }
 }
 
--- Demo1to1MappingTestModule/Production_test/ProductFeatures/DevelopUtil/ObjectUtils.Tests.ps1
+++ Demo1to1MappingTestModule/Production_test/ProductFeatures/DevelopUtil/ObjectUtils.Tests.ps1
@@ -15,6 +15,16 @@ Describe "Echo function API declaration" -Tag "EchoFeature", "FunctionDeclaratio
                 }
                 Outputs     = @([System.Type]) # No strictly specified output type for this function
             }
+            @{
+                Name        = 'Dump-ObjectProps';
+                CommandType = [System.Management.Automation.CommandTypes]::Function;
+                Inputs      = @{
+                    Value            = [object]
+                    'Type'           = [System.Type]
+                    PropertiesFilter = [string[]]
+                }
+                Outputs     = @([string[]])
+            }
         )
     }
     BeforeAll {
@@ -48,5 +58,49 @@ Describe "SafelyGetType()" -Tag "ObjectUtils" {
         $testInput = [PSCustomObject]@{Name = "TestName"; Value = "TestValue" }
         $result = SafelyGetType -obj $testInput
         $result | Should -BeExactly $testInput.GetType()
+    }
+}
+
+Describe "Dump-ObjectProps" -Tag "ObjectUtils" {
+
+    It "Should return properties of input string" {
+        $testInput = "TestString"
+        $result = Dump-ObjectProps -Value $testInput
+        $result | Should -Contain "Length=10"
+    }
+
+    It "Should return properties of input array" {
+        $testInput = @()
+        $result = Dump-ObjectProps -Value $testInput
+        $result | Should -Contain "Length=0"
+        $result | Should -Contain "Rank=1"
+    }
+
+    It "Should return properties of custom PSObject input " {
+        $testInput = [PSCustomObject]@{Name = "TestName"; Value = "TestValue" }
+        $result = Dump-ObjectProps -Value $testInput
+        $result | Should -Contain "Name=TestName"
+        $result | Should -Contain "Value=TestValue"
+    }
+
+    It "Should return properties of custom PSObject input with type" {
+        $testInput = [PSCustomObject]@{Name = "TestName"; Value = "TestValue" }
+        $result = Dump-ObjectProps -Value $testInput -Type ([System.Type]::GetType("System.String"))
+        $result | Should -BeNullOrEmpty
+    }
+
+    It "Should return properties of custom PSObject input with type" {
+        $testInputValue = [PSCustomObject]@{Name = "TestName"; Value = "TestValue" }
+        $result = Dump-ObjectProps -Value $testInputValue -PropertiesFilter "Name"
+        $result | Should -Contain "Name=TestName"
+        $result | Should -Not -Contain "Value=TestValue"
+    }
+
+    It "Shoulld return properties of Hashtable input" {
+        $testInput = [Ordered]@{Name = "TestName"; Value = "TestValue" }
+        $result = Dump-ObjectProps -Value $testInput
+        $result | Should -Contain "Count=2"
+        $result | Should -Contain "Keys=Name Value"
+        $result | Should -Contain "Values=TestName TestValue"
     }
 }
