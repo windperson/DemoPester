db2bf1ca4e2e572f1facb5657c46cfc044c4d480
Isak Pao <windperson@gmail.com>
2024-10-23 23:36:08 +0800

    Add function to list OS info
    Add some utility scripts
    
    remove switch extension setting

---
+++ Demo1to1MappingTestModule/Production/ProductFeatures/DevelopUtil/OsInfo.ps1
@@ -0,0 +1,51 @@
+﻿function Get-OSInfo {
+    if ( $IsWindows) {
+        $cim = Get-CimInstance -ClassName Win32_OperatingSystem
+        $osInfo = @{
+            OSName       = $cim.Caption
+            Version      = $cim.Version
+            Build        = $cim.BuildNumber
+            Architecture = $cim.OSArchitecture
+        }
+    }
+    elseif ( $IsLinux ) {
+        if (Get-Command hostnamectl) {
+            $info = hostnamectl --json=short | ConvertFrom-Json
+            $osName = $info.OperatingSystemPrettyName
+        }
+        else {
+            $osName = uname -s
+        }
+        $osVersion = Select-String -Path /etc/*release -Pattern "VERSION=`"(.*)`"" | ForEach-Object { $_.Matches[0].Groups[1].Value }
+        $osRelease = uname -r
+        $arch = uname -p
+        $osInfo = @{
+            OSName       = $osName
+            Version      = $osVersion
+            Build        = $osRelease
+            Architecture = $arch
+        }
+    }
+    elseif ( $IsMacOS ) {
+        $os_name = sw_vers --productName
+        $os_version = sw_vers --productVersion
+        $os_build = sw_vers --buildVersion
+        $arch = sysctl -n machdep.cpu.brand_string
+        $osInfo = @{
+            OSName       = $os_name
+            Version      = $os_version
+            Build        = $os_build
+            Architecture = $arch
+        }
+    }
+    else {
+        $osInfo = @{
+            OSName       = ""
+            Version      = ""
+            Build        = ""
+            Architecture = ""
+        }
+    }
+
+    return $osInfo
+}
---
+++ DemoTestWithMock/CreateMsBuildConfigFile/CreateDirectoryBuildProps.ps1
@@ -0,0 +1,19 @@
+﻿param (
+    [string]$FilePath = ".",
+    [string]$FileName = "Directory.Build.props"
+)
+# MSBuild config XML content
+$xmlContent = @"
+<Project>
+    <PropertyGroup Condition=" '`$(Configuration)|`$(Platform)' == 'Release|x64' ">
+        <DebugType>pdbonly</DebugType>
+    </PropertyGroup>
+</Project>
+"@
+
+$targetFilePath = Join-Path $FilePath $FileName
+# Write the XML content to Directory.Build.props
+$xmlContent | Out-File -FilePath $targetFilePath -Encoding utf8
+if (-not (Test-Path -Path $targetFilePath)) {
+    throw "try to save `"$targetFilePath`" failed"
+}
