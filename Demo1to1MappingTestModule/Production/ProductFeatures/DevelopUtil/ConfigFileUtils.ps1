a3da7e6a51c5239b4dc3cf3021efb59b4e009154
Isak Pao <windperson@gmail.com>
2024-10-24 17:23:17 +0800

    file comment line utility
    AAdd simple file comment line script

---
+++ Demo1to1MappingTestModule/Production/ProductFeatures/DevelopUtil/ConfigFileUtils.ps1
@@ -0,0 +1,18 @@
+﻿function Set-FileLineComment {
+    [CmdletBinding(SupportsShouldProcess)]
+    param(
+        [string]$filePath,
+        [string]$searchPattern,
+        [string]$commentText = '#'
+    )
+
+    if (-not(Test-Path $filePath)) {
+        throw "File not exist"
+    }
+
+    $replaceStr = "$commentText`$1"
+
+    if ($PSCmdlet.ShouldProcess($filePath)) {
+        (Get-Content -Path $filePath) -replace "^(\s*$searchPattern\s*)`$", $replaceStr | Set-Content -Path $filePath
+    }
+}
---
+++ Demo1to1MappingTestModule/Production_test/ProductFeatures/DevelopUtil/Test.ps1
@@ -0,0 +1,26 @@
+﻿### NOTE: thia is A demo ps1 script for being manipulated by ConfigFileUtils.ps1
+### DO NOT REALLY RUN this script!!!
+#<#
+.SYNOPSIS
+
+
+.DESCRIPTION
+Long description
+
+.EXAMPLE
+An example
+
+.NOTES
+General notes
+#>
+function RemoveDebugSymbol {
+    parma(
+        [string]$projectDir,
+        [string]$buildConfiguration = 'Release'
+    )
+
+    if ($buildConfiguration -eq "Release") {
+        remove-item -Recurse "$projectDir\**\bin\*.pdb"
+        remove-item -Recurse "$projectDir\**\obj\*.obj"
+    }
+}
