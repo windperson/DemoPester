f4221e6670a8e131b0348de093d77ddbb8f16c2f
Isak Pao <windperson@gmail.com>
2024-11-08 00:33:00 +0800

    Add InModuleScope block demo test case

---
+++ DemoScope/PesterScopeDemo.Tests.ps1
@@ -0,0 +1,34 @@
+﻿BeforeAll {
+    $myVar = "This is a semi global variable"
+}
+
+Describe "testing PesterScopeDemo.psm1" {
+    BeforeAll {
+        # Remove-Module -Name PesterScopeDemo -Verbose -ErrorAction Continue
+        Import-Module "$PSScriptRoot/PesterScopeDemo.psm1" -Verbose -Force
+    }
+    Context "test Get-Greeting() function without Mocking" {
+        It "default value" {
+            Get-Greeting | Should -Be "Hello, World!"
+        }
+        It "customer GreeterName" {
+            Set-GreeterName -GreeterName "Pester"
+            Get-Greeting | Should -Be "Hello, Pester!"
+        }
+    }
+    Context "test Set-GreeterName() function with Mocking" {
+        It "use InModuleScope to verify script level variable" {
+            # Arrange
+            Set-Variable MyDemoGreeter -Scope global -Option Constant `
+                -Value 'Pester_InModuleScope'
+
+            # Act
+            Set-GreeterName -GreeterName $MyDemoGreeter
+            # Assert
+            InModuleScope 'PesterScopeDemo' {
+                $script:name | Should -Be $MyDemoGreeter
+            }
+        }
+    }
+}
+
---
+++ DemoScope/PesterScopeDemo.psm1
@@ -0,0 +1,17 @@
+﻿$script:name = "World"
+
+function Get-Greeting {
+    $greeting = "Hello, $script:name!"
+    return $greeting
+}
+
+function Set-GreeterName {
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseShouldProcessForStateChangingFunctions', '')]
+    param(
+        [Parameter(Mandatory)]
+        [string]
+        $GreeterName
+    )
+
+    $script:name = $GreeterName
+}
