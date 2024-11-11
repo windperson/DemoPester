0ee13e441dd532c83582b04e2eda20db6134dfe4
Isak Pao <windperson@gmail.com>
2024-11-11 10:23:15 +0800

    Add more precise DateTimeFormat script
    remove too easily check part

--- Demo1to1MappingTestModule/Production/ProductFeatures/StringFeatureModule/FormatDateTime.ps1
+++ Demo1to1MappingTestModule/Production/ProductFeatures/StringFeatureModule/FormatDateTime.ps1
@@ -1,8 +1,28 @@
-﻿param(
+﻿#Requires -Version 7
+param(
     [string] $formatterString = 'yyyy-MM-dd HH:mm:ss'
 )
 
-$formatter = $formatterString
+function CorrectInput($formatterString) {
+    Write-Information "CorrectInput() declared in `"$(Split-Path -Leaf $PSCommandPath)`", input `$formatterString=$formatterString" `
+        -InformationAction Continue
+
+    function IsValidFormat {
+        param(
+            [string] $formatStr
+        )
+        if ([String]::IsNullOrEmpty($formatStr)) {
+            return $false
+        }
+
+        return $true
+    }
+
+    return (IsValidFormat $formatterString)? $formatterString : 'yyyy-MM-dd HH:mm:ss'
+}
+
+$formatter = CorrectInput($formatterString)
+
 function Get-FixedFormattedDateTimeNow {
     [OutputType([string])]
     param()
