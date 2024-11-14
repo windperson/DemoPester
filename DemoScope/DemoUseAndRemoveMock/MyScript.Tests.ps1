f49873eeffb1542b91133659a12dbb6301009e85
Isak Pao <windperson@gmail.com>
2024-11-14 18:44:19 +0800

    Add manual remove mock example test case

---
+++ DemoScope/DemoUseAndRemoveMock/MyScript.Tests.ps1
@@ -0,0 +1,25 @@
+﻿#Requires -Version 7
+Describe "MyFunction" {
+    BeforeAll {
+        Import-Module "$PSScriptRoot/MyScript.psm1" -Verbose -Force
+    }
+    It "should return the mocked time" {
+        InModuleScope MyScript {
+            Mock -CommandName Get-CurrentTime -MockWith {
+                $output = "2024-11-14 08:00:00"
+                Write-Information -MessageData "In Mocked Get-CurrentTime(), output=$output" -InformationAction Continue
+                return $output
+            }
+
+            $result = MyDateTime
+            $result | Should -Be "The current time is 2024-11-14 08:00:00"
+
+            # Remove mocked Get-CurrentTime Function
+            Remove-Alias Get-CurrentTime
+        }
+
+        # Call the function again and check the result without the mock
+        $result = MyDateTime
+        $result | Should -Not -Be "The current time is 2024-11-14 08:00:00"
+    }
+}
---
+++ DemoScope/DemoUseAndRemoveMock/MyScript.psm1
@@ -0,0 +1,16 @@
+﻿function Get-CurrentTime {
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSProvideCommentHelp', '')]
+    param()
+    $output = (Get-Date).ToString()
+    Write-Information -MessageData "In Real Get-CurrentTime() function, `$output=$output" -InformationAction Continue
+    return $output
+}
+
+function MyDateTime {
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSProvideCommentHelp', '')]
+    param()
+    $time = Get-CurrentTime
+    return "The current time is $time"
+}
+
+Export-ModuleMember MyDateTime
