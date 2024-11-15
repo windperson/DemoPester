3d09f132b2efbbd96724c9caeaad20611ab676ba
Isak Pao <windperson@gmail.com>
2024-11-15 12:18:01 +0800

    Add more filter test cases

--- DemoTestWithMock/PipelineFilter/MyCustomFilters.Tests.ps1
+++ DemoTestWithMock/PipelineFilter/MyCustomFilters.Tests.ps1
@@ -21,3 +21,35 @@
         }
     }
 }
+
+Describe "Test IsSmallerThen/IsLargerThen filter" {
+    BeforeAll {
+        Set-Variable -Name "SourcetNumbers" -Option Constant -Value (1..20)
+        Import-Module "$PSScriptRoot\MyCustomFilters.psm1" -Force
+    }
+
+    It "IsSmallerThen filter Should return values smaller than or equal to 10 when UpLimit is 10" {
+        $expect = 1..10
+        $result = $SourcetNumbers | IsSmallerThen -UpLimit 10
+        $result | Should -BeExactly $expect
+    }
+
+    It "Should return values Larger than or equal to 10 when LowLimit is 10" {
+        $expect = 10..20
+        $result = $SourcetNumbers | IsLargerThen -LowLimit 10
+        $result | Should -BeExactly $expect
+    }
+}
+
+Describe "Test IsInRange filter"{
+    BeforeAll {
+        Set-Variable -Name "SourcetNumbers" -Option Constant -Value (1..20)
+        Import-Module "$PSScriptRoot\MyCustomFilters.psm1" -Force
+    }
+
+    It "Should return 10, 11, 12, 13, 14, 15 when set LowerLimt=10, UpperLimit=15" {
+        $expect = 10..15
+        $result = $SourcetNumbers | IsInTheRange -LowerLimit 10 -UpperLimit 15
+        $result | Should -BeExactly $expect
+    }
+}
--- DemoTestWithMock/PipelineFilter/MyCustomFilters.psm1
+++ DemoTestWithMock/PipelineFilter/MyCustomFilters.psm1
@@ -12,18 +12,28 @@ filter IsEvenNumber {
 
 filter IsSmallerThen {
     param(
-        [int] $TopLimit
+        [int] $UpLimit
     )
-    if ($_ -le $TopLimit) {
+    if ($_ -le $UpLimit) {
         return $_
     }
 }
 
-function IsLargerThen {
+filter IsLargerThen {
     param (
         [int] $LowLimit
     )
     if ($_ -ge $LowLimit) {
+        return $_
+    }
+}
+
+filter IsInTheRange {
+    param(
+        [int] $LowerLimit,
+        [int] $UpperLimit
+    )
+    $_ | IsLargerThen -LowLimit $LowerLimit | IsSmallerThen -UpLimit $UpperLimit | ForEach-Object {
         return $_
     }
 }
