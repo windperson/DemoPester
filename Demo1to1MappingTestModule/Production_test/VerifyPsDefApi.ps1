973a6e9de9a01a5d4321b3ed7677807f9f7590ad
Isak Pao <windperson@gmail.com>
2024-12-10 20:12:19 +0800

    Add duplicate file finder module
    PSCustomObject arrary instead of Custom type array
    Because the Get-Command cmdlet cannot correct get the Parameter of a command with custom defined class type inside the module.
    
    Add test code for DuplicateFilesFinder

--- Demo1to1MappingTestModule/Production_test/VerifyPsDefApi.ps1
+++ Demo1to1MappingTestModule/Production_test/VerifyPsDefApi.ps1
@@ -1,4 +1,8 @@
+#region Script Requirement settings
 #Requires -Version 7
+#Requires -Module @{ ModuleName='Pester'; ModuleVersion="5.6.1"}
+#endregion
+
 function VerifyApiDefinition {
     <#
     .SYNOPSIS
